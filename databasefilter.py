0f78b3505f38b11de096d8e6523b6dbd653f55f7
Deonte <dawsondeonte@gmail.com>
2024-09-30 19:12:15 -0400

    first commit

---
+++ databasefilter.py
@@ -0,0 +1,16 @@
+# Download the databse file of database.db.  The file contains a table with 50 country names with their area in square km in population.
+#Use Python to print out those countries that have an area of greater than 2,000,000 km^2
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+cur = conn.cursor()
+# cur.execute("SELECT country FROM countries WHERE area >= 2000000")
+cur.execute("SELECT * FROM countries")
+rows = cur.fetchall()
+conn.close()
+# print(rows)
+
+for i in rows:
+    print(i[0])
+    
+
